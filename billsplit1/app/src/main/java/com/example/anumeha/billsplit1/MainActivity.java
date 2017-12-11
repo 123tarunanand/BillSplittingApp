5220eeb884790e279ff2ce0f50b1ab8b678c1c86
SanjanaKrishnam <33081854+SanjanaKrishnam@users.noreply.github.com>
2017-12-11 23:57:06 +0530

    Add files via upload

--- billsplit1/app/src/main/java/com/example/anumeha/billsplit1/MainActivity.java
+++ billsplit1/app/src/main/java/com/example/anumeha/billsplit1/MainActivity.java
@@ -1,54 +1,51 @@
-package com.example.anumeha.billsplit1;
+package com.example.sanjana.codesprint;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.*;
+import java.util.*;
+import android.view.*;
 
-/** Note that here we are inheriting ListActivity class instead of Activity class **/
-public class MainActivity extends ListActivity {
-    /** Items entered by the user is stored in this ArrayList variable */
-    ArrayList<String> list = new ArrayList<String>();
-    /** Declaring an ArrayAdapter to set items to ListView */
-    ArrayAdapter<String> adapter;
-    HashMap<String,Integer> cartlist= new HashMap<>();
+public class MainActivity extends Activity {
+
+    ListView listview;
+    Button Addbutton;
+    EditText GetValue;
+    EditText GetValue1;
+
+
+
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /** Setting a custom layout for the list activity */
         setContentView(R.layout.activity_main);
-        /** Reference to the button of the layout main.xml */
-        Button btn = (Button) findViewById(R.id.btnAdd);
-        /** Defining the ArrayAdapter to set items to ListView */
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
-        /** Defining a click event listener for the button "Add" */
-        btn.setOnClickListener(new OnClickListener() {
+
+        listview = (ListView) findViewById(R.id.listView1);
+        Addbutton = (Button) findViewById(R.id.button1);
+        GetValue = (EditText) findViewById(R.id.editText1);
+        GetValue1 = (EditText) findViewById(R.id.editText2);
+
+        final ArrayList<Item> ItemListelements=new ArrayList<Item>();
+        final CustomAdapter adapter = new CustomAdapter(this,ItemListelements);
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        listView.setEmptyView(findViewById(R.id.empty));
+        listview.setAdapter(adapter);
+        Addbutton.setOnClickListener(new View.OnClickListener()
+        {
+
             @Override
             public void onClick(View v) {
-                EditText edit = (EditText) findViewById(R.id.txtItem);
-                list.add(edit.getText().toString());
-                edit.setText(" ");
+
+                Item new_item=new Item(GetValue.getText().toString(),Integer.parseInt(GetValue1.getText().toString()));
+                ItemListelements.add(new_item);
                 adapter.notifyDataSetChanged();
+                GetValue.setText("");
+                GetValue1.setText("");
+
             }
         });
-        setListAdapter(adapter);
-
-    }
-    public void Additemprice(ArrayList<String> arraylist){
-        for(int i=0;i<arraylist.size();i++){
-            String item=arraylist.get(i);
-            String array1[]=item.split(" ");
-            cartlist.put(array1[0],Integer.parseInt(array1[1]));
-            EditText edit = (EditText) findViewById(R.id.txtItem);
-            edit.setText(cartlist.get(array1[0]));
-        }
-
+        listview.setAdapter(adapter);
     }
 }
