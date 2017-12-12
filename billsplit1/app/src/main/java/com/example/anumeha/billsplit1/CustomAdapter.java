9bbe68be6121d0adcc423fefe9f0f6745dd220a8
SanjanaKrishnam <33081854+SanjanaKrishnam@users.noreply.github.com>
2017-12-12 09:34:41 +0530

    Update CustomAdapter.java

--- billsplit1/app/src/main/java/com/example/anumeha/billsplit1/CustomAdapter.java
+++ billsplit1/app/src/main/java/com/example/anumeha/billsplit1/CustomAdapter.java
@@ -1,10 +1,4 @@
-package com.example.sanjana.codesprint;
-
-/**
- * Created by Sanjana on 11/12/17.
- */
-
-
+package com.example.sanjana.bill;
 import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
 import android.os.Bundle;
@@ -13,6 +7,10 @@ import java.util.*;
 import android.view.*;
 import android.content.*;
 
+
+/**
+ * Created by Sanjana on 11/12/17.
+ */
 
 public class CustomAdapter extends BaseAdapter {
     Context context;
@@ -40,13 +38,23 @@ public class CustomAdapter extends BaseAdapter {
             LayoutInflater mInflater = (LayoutInflater) context
                     .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
             convertView = mInflater.inflate(R.layout.item, null);
-            TextView item = (TextView) convertView.findViewById(R.id.item);
-            TextView price = (TextView) convertView.findViewById(R.id.price);
+            TextView item = (TextView) convertView.findViewById(R.id.ItemName);
+            TextView price = (TextView) convertView.findViewById(R.id.Price);
+            ImageView imgRemove = (ImageView) convertView.findViewById(R.id.imgRemove);
             Item m = itemModelList.get(position);
             item.setText(m.getItem());
             price.setText(m.getPrice().toString());
 
+            imgRemove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    itemModelList.remove(position);
+                    notifyDataSetChanged();
+                }
+            });
         }
         return convertView;
     }
 }
+
+
