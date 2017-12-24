9850155a9e29be54c29225381566ba191748d85c
123tarunanand <anandtarun2@gmail.com>
2017-12-24 22:59:30 +0530

    hi

--- BILL/app/src/main/java/com/example/sanjana/bill/Thirdact.java
+++ BILL/app/src/main/java/com/example/sanjana/bill/Thirdact.java
@@ -6,10 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
 public class Thirdact extends AppCompatActivity {
     TextView head;
     String title;
     ListView listView;
+    ArrayList<Item> ItemModelList;
+    HashMap<String,Double> FinalDisplay;
+    CustomAdapter customAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +27,18 @@ public class Thirdact extends AppCompatActivity {
         title = args.getString("NAME");
         head.setText(title);
         listView = (ListView)findViewById(R.id.litsy);
+        FinalDisplay=(HashMap<String,Double>)args.getSerializable("FinalDisplay");
+        ItemModelList = new ArrayList<Item>();
+        customAdapter = new CustomAdapter(this, ItemModelList);
+        listView.setAdapter(customAdapter);
+        Set<String> keys = FinalDisplay.keySet();
+        for(String key : keys)
+        {
+            Item it = new Item(key, FinalDisplay.get(key));
+            ItemModelList.add(it);
+            customAdapter.notifyDataSetChanged();
+        }
+
 
     }
 }
